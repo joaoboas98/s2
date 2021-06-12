2116c21880457372ddfd5a3fe864be2cb02d629e
joaoboas98 <62546037+joaoboas98@users.noreply.github.com>
2021-06-12 16:08:47 +0100

    login .l.

---
+++ src/main/java/SpringWebMVC/s2/controller/utilizador.java
@@ -0,0 +1,55 @@
+package SpringWebMVC.s2.controller;
+
+import SpringWebMVC.s2.DAL.Utilizador;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class utilizador {
+    @RequestMapping(value= "/login")
+
+    public ModelAndView test(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        String pass = request.getParameter("Pass");
+        String nome = request.getParameter("Uname");
+
+        SpringWebMVC.s2.DAL.Utilizador modelInstance = SpringWebMVC.s2.BLL.Utilizador.UtilizadorLogin(nome,pass);
+
+        ModelAndView mview = null;
+        String msg = "";
+
+        if (modelInstance == null) {
+            msg = "Credenciais erradas!";
+            mview = new ModelAndView("index");
+            mview.addObject("msg", msg);
+            System.out.println("null");
+
+        } else if (modelInstance.getUtilizadorTipo().equals(1)) {
+            System.out.println("utilizador");
+            if (modelInstance.getUtilizadorId() == null) {
+                System.out.println("Novo Utilizador");
+                mview = new ModelAndView("home");
+                mview.addObject("utilizador", modelInstance);
+
+                mview.addObject("msg", msg);
+            }
+        } else if (modelInstance.getUtilizadorTipo().equals(0)) {
+            System.out.println("Admin");
+            mview = new ModelAndView("home");
+            mview.addObject("utilizador", modelInstance);
+            mview.addObject("msg", msg);
+        }
+
+        return mview;
+
+
+    }
+
+
+}
